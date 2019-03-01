24ae0c8fb75abf7dff42428e657fd57d95d97dd8
Audra Tallman <audra.tallman@hotmail.com>
2019-03-01 20:17:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,20 +15,15 @@ def pop_with_args (array)
 end
 
 def using_shift(array)
-  array.shift
+   array.shift
 end
 
 def shift_with_arts(array)
-   @ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
-    @brands_removed = shift_with_args(@ice_cream_brands)
-  end
+    array.shift(2)
+end
 
 def using_concat (array1, array2)
-  @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
-    more_favs = ["mario kart", "flatiron school"]
-    @all_my_favs = using_concat(@my_favorite_things, more_favs)
-  @all_my_favs
-  @my_favorite_things.length
+  
 end
 
 def using_insert(array, element)
